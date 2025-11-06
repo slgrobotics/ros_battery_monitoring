635043eec933e5e9169ab7d871b9397979a27e05
Sergei Grichine <msghub@hotmail.com>
2025-11-05 21:20:43 -0600

    Changes to make Jazzy code also Kilted-compatible

--- battery_state_broadcaster/src/BatteryStateBroadcaster.cpp
+++ battery_state_broadcaster/src/BatteryStateBroadcaster.cpp
@@ -31,7 +31,11 @@ BatteryStateBroadcaster::on_configure(const rclcpp_lifecycle::State& /*previous_
 
   params_ = param_listener_->get_params();
 
-  battery_sensor_ = std::make_unique<BatterySensor>(BatterySensor(sensor_name, params_.state_interfaces, get_node()->get_logger()));
+  // Old way of constructing BatterySensor in Jazzy:
+  //battery_sensor_ = std::make_unique<BatterySensor>(BatterySensor(sensor_name, params_.state_interfaces, get_node()->get_logger()));
+
+  // In Kilted some controller plugin types (like BatterySensor) are now non-copyable / non-movable. Works in Jazzy too:
+  battery_sensor_ = std::make_unique<BatterySensor>(sensor_name, params_.state_interfaces, get_node()->get_logger());
 
   try {
     // register ft sensor data publisher
@@ -46,25 +50,37 @@ BatteryStateBroadcaster::on_configure(const rclcpp_lifecycle::State& /*previous_
     return CallbackReturn::ERROR;
   }
 
-  realtime_publisher_->lock();
+  //
+  // A better Kilted way of filling message:
+  //
+  // realtime_publisher_->try_publish([&](auto &msg) {
+  //   msg.design_capacity = static_cast<float>(params_.design_capacity);
+  //   ...
+  // });
+  sensor_msgs::msg::BatteryState msg;
 
+  // A Jazzy and Kilted compatible way of filling message in the interim:
   // seed the message with static values from parameters and defaults for dynamic members:
-  realtime_publisher_->msg_.header.frame_id = params_.frame_id;
-  realtime_publisher_->msg_.design_capacity = params_.design_capacity;
-  realtime_publisher_->msg_.voltage = std::numeric_limits<float>::quiet_NaN();
-  realtime_publisher_->msg_.temperature = std::numeric_limits<float>::quiet_NaN();
-  realtime_publisher_->msg_.charge = std::numeric_limits<float>::quiet_NaN();
-  realtime_publisher_->msg_.current = std::numeric_limits<float>::quiet_NaN();
-  realtime_publisher_->msg_.capacity = std::numeric_limits<float>::quiet_NaN();
-  realtime_publisher_->msg_.percentage = std::numeric_limits<float>::quiet_NaN();
-  realtime_publisher_->msg_.power_supply_status = sensor_msgs::msg::BatteryState::POWER_SUPPLY_STATUS_UNKNOWN;
-  realtime_publisher_->msg_.power_supply_health = sensor_msgs::msg::BatteryState::POWER_SUPPLY_HEALTH_UNKNOWN;
-  realtime_publisher_->msg_.power_supply_technology = params_.power_supply_technology;
-  realtime_publisher_->msg_.present = false;
-  realtime_publisher_->msg_.location = params_.location;
-  realtime_publisher_->msg_.serial_number = params_.serial_number;
+  msg.header.frame_id = params_.frame_id;
+  msg.design_capacity = static_cast<float>(params_.design_capacity);
+  msg.voltage = std::numeric_limits<float>::quiet_NaN();
+  msg.temperature = std::numeric_limits<float>::quiet_NaN();
+  msg.charge = std::numeric_limits<float>::quiet_NaN();
+  msg.current = std::numeric_limits<float>::quiet_NaN();
+  msg.capacity = std::numeric_limits<float>::quiet_NaN();
+  msg.percentage = std::numeric_limits<float>::quiet_NaN();
+  msg.power_supply_status = sensor_msgs::msg::BatteryState::POWER_SUPPLY_STATUS_UNKNOWN;
+  msg.power_supply_health = sensor_msgs::msg::BatteryState::POWER_SUPPLY_HEALTH_UNKNOWN;
+  msg.power_supply_technology = params_.power_supply_technology;
+  msg.present = false;
+  msg.location = params_.location;
+  msg.serial_number = params_.serial_number;
 
-  realtime_publisher_->unlock();
+  if(!realtime_publisher_->try_publish(msg)) {
+    RCLCPP_ERROR(
+      get_node()->get_logger(), "Failed to lock realtime publisher at configure stage \n");
+    return CallbackReturn::ERROR;
+  } 
 
   RCLCPP_DEBUG(get_node()->get_logger(), "on_configure() successful");
 
@@ -104,11 +120,12 @@ BatteryStateBroadcaster::on_deactivate(const rclcpp_lifecycle::State& /*previous
 controller_interface::return_type BatteryStateBroadcaster::update(const rclcpp::Time& time,
                                                                   const rclcpp::Duration& /*period*/)
 {
-  if (realtime_publisher_ && realtime_publisher_->trylock())
-  {
-    realtime_publisher_->msg_.header.stamp = time;
-    battery_sensor_->populate_message_from_interfaces(realtime_publisher_->msg_);
-    realtime_publisher_->unlockAndPublish();
+  sensor_msgs::msg::BatteryState msg;
+
+  if (realtime_publisher_) {
+    msg.header.stamp = time;
+    battery_sensor_->populate_message_from_interfaces(msg);
+    realtime_publisher_->try_publish(msg);
   }
 
   return controller_interface::return_type::OK;
