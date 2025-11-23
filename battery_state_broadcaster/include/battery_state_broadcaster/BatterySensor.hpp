7d66fa0e5d08ae895025aa000dba4f446afdbbbf
Dmitrii Gusev <amidg4x4dima@gmail.com>
2025-11-23 14:02:00 -0800

    compiles against jazzy

--- battery_state_broadcaster/include/battery_state_broadcaster/BatterySensor.hpp
+++ battery_state_broadcaster/include/battery_state_broadcaster/BatterySensor.hpp
@@ -14,7 +14,8 @@ namespace battery_state_broadcaster
 class BatterySensor : public semantic_components::SemanticComponentInterface<sensor_msgs::msg::BatteryState>
 {
 public:
-  explicit BatterySensor(const std::string& name, const std::vector<std::string>& interfaces)
+  explicit BatterySensor(
+    const std::string& name, const std::vector<std::string>& interfaces)
     : semantic_components::SemanticComponentInterface<sensor_msgs::msg::BatteryState>(name, interfaces.size())
   {
     for (const auto& interface : interfaces)
@@ -26,54 +27,53 @@ public:
 
   virtual ~BatterySensor() = default;
 
-  double get_voltage()
-  {
-    voltage_ = state_interfaces_[0].get().get_value();
-    return voltage_;
-  }
-
   void populate_message_from_interfaces(sensor_msgs::msg::BatteryState& message)
   {
-    // seed the message with dynamic values - only from existing interfaces:
+    // do this only for the existing interfaces
     for (const auto& state_interface : state_interfaces_)
     {
       const auto& name = state_interface.get().get_interface_name();
-      const auto& value = state_interface.get().get_value();
-      if (name == "voltage")
-      {
-        message.voltage = value;
-      }
-      else if (name == "temperature")
-      {
-        message.temperature = value;
-      }
-      else if (name == "charge")
-      {
-        message.charge = value;
-      }
-      else if (name == "current")
-      {
-        message.current = value;
-      }
-      else if (name == "capacity")
-      {
-        message.capacity = value;
-      }
-      else if (name == "percentage")
-      {
-        message.percentage = value;
-      }
-      else if (name == "power_supply_health")
-      {
-        message.power_supply_health = static_cast<uint8_t>(std::round(value));
-      }
-      else if (name == "power_supply_status")
-      {
-        message.power_supply_status = static_cast<uint8_t>(std::round(value));
-      }
-      else if (name == "present")
-      {
-        message.present = static_cast<int>(std::round(value)) == 0 ? false : true;
+      if (const auto& data = state_interface.get().get_optional(1); data != std::nullopt) {
+          if (name == "voltage")
+          {
+            message.voltage = data.value();
+          }
+          else if (name == "temperature")
+          {
+            message.temperature = data.value();
+          }
+          else if (name == "charge")
+          {
+            message.charge = data.value();
+          }
+          else if (name == "current")
+          {
+            message.current = data.value();
+          }
+          else if (name == "capacity")
+          {
+            message.capacity = data.value();
+          }
+          else if (name == "percentage")
+          {
+            message.percentage = data.value();
+          }
+          else if (name == "power_supply_health")
+          {
+            message.power_supply_health =
+                static_cast<uint8_t>(std::round(data.value()));
+          }
+          else if (name == "power_supply_status")
+          {
+            message.power_supply_status =
+                static_cast<uint8_t>(std::round(data.value()));
+          }
+          else if (name == "present")
+          {
+            message.present =
+                static_cast<int>(
+                    std::round(data.value())) == 0 ? false : true;
+          }
       }
     }
   }
